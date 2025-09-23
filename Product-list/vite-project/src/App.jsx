2e4018ff30598b2eb9472ca895777311517ab0cb
Lucky-1300 <luckykumari42774@gmail.com>
2025-09-23 15:14:03 +0530

    chnaged

---
+++ Product-list/vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import ProductList from "./assets/Task-2/ProductList"
+
+function App() {
+
+
+  return (
+    <>
+     <ProductList/>
+    </>
+  )
+}
+
+export default App;
---
+++ Product-list/vite-project/src/assets/Task-2/ProductList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ProductList.css'
+
+const ProductList = () => {
+  return (
+    <div>
+    <h1>Lucky Ray</h1>
+    </div>
+  )
+}
+
+export default ProductList
