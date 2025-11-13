58cc5c28301d779ad6bf973e5ffd6bd9d0f29fb0
Lucky-1300 <youremail@example.com>
2025-11-13 14:07:33 +0530

    done

---
+++ ConvertApp/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-400'>khiufevb</div>
+      <h1 className='bg-pink-600'>Lucky</h1>
+    </>
+  )
+}
+
+export default App
